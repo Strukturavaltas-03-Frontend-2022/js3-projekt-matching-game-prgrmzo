e12e6d620ae3c29f1ef5c414b7ce4484f9eeb29d
Zo <pahyzoard@outlook.hu>
2022-09-02 00:18:22 +0200

    timer needs fixing.

---
+++ script.js
@@ -0,0 +1,137 @@
+"use strict";
+
+const section = document.querySelector("section");
+
+const getData = () => [
+  { cardSrc: "./assets/svgs/bulbasaur.svg", name: "bulbasaur" },
+  { cardSrc: "./assets/svgs/charmander.svg", name: "charmander" },
+  { cardSrc: "./assets/svgs/eevee.svg", name: "eevee" },
+  { cardSrc: "./assets/svgs/pikachu.svg", name: "pikachu" },
+  { cardSrc: "./assets/svgs/squirtle.svg", name: "squirtle" },
+  { cardSrc: "./assets/svgs/bulbasaur.svg", name: "bulbasaur" },
+  { cardSrc: "./assets/svgs/charmander.svg", name: "charmander" },
+  { cardSrc: "./assets/svgs/eevee.svg", name: "eevee" },
+  { cardSrc: "./assets/svgs/pikachu.svg", name: "pikachu" },
+  { cardSrc: "./assets/svgs/squirtle.svg", name: "squirtle" },
+];
+
+const randomize = () => {
+  const cardData = getData();
+  cardData.sort(() => Math.random() - 0.5);
+  return cardData;
+};
+
+randomize();
+
+const cardGenerator = () => {
+  const cardData = randomize();
+
+  cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+
+    card.classList = "card";
+    face.classList = "face";
+    back.classList = "back";
+
+    face.src = item.cardSrc;
+    card.setAttribute("name", item.name);
+
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+
+    card.addEventListener("click", (e) => {
+      card.classList.toggle("toggleCard");
+      matchChecker(e);
+    });
+  });
+};
+
+const matchChecker = (e) => {
+  const clickedCard = e.target;
+  clickedCard.classList.add("flipped");
+  const flippedCards = document.querySelectorAll(".flipped");
+  const toggleCard = document.querySelectorAll(".toggleCard");
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        card.style.pointerEvents = "none";
+      });
+    } else {
+      flippedCards.forEach((card) => {
+        card.classList.remove("flipped");
+        setTimeout(() => card.classList.remove("toggleCard"), 1000);
+      });
+    }
+  }
+  if (toggleCard.length === 10) {
+    stopTimer();
+    setTimeout(() => restart(), 5000);
+    setTimeout(() => resetTimer(), 5000);
+  }
+};
+
+const restart = () => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll(".face");
+  let cards = document.querySelectorAll(".card");
+  section.style.pointerEvents = "none";
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove("toggleCard");
+
+    setTimeout(() => {
+      cards[index].style.pointerEvents = "all";
+      faces[index].src = item.cardSrc;
+      cards[index].setAttribute("name", item.name);
+      section.style.pointerEvents = "all";
+    }, 500);
+  });
+};
+
+cardGenerator();
+
+// Timer
+
+let seconds = 0;
+let interval = null;
+
+const timeCounter = document.querySelector(".time");
+
+const timer = () => {
+  let secs = seconds % 60;
+  let mins = Math.floor(seconds / 60);
+
+  if (secs < 10) {
+    secs = `0${secs}`;
+  }
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  timeCounter.innerHTML = `${mins}:${secs}`;
+  seconds++;
+};
+
+const startTimer = () => {
+  if (interval) {
+    return;
+  }
+  interval = setInterval(timer, 1000);
+};
+
+const stopTimer = () => {
+  clearInterval(interval);
+};
+
+const resetTimer = () => {
+  stopTimer();
+  seconds = 0;
+  timeCounter.innerHTML = "00:00";
+};
+
+section.addEventListener("click", startTimer);
